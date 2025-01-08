3a133d91586441bf4c46f38ddb75ad7c55727285
Purnendu-sarkar <dsr102.purnendu@gmail.com>
2025-01-08 22:59:00 +0600

    Set Up Done.

---
+++ src/pages/Home/Home/Home.jsx
@@ -0,0 +1,12 @@
+
+
+const Home = () => {
+    return (
+        <div>
+            <h1>Welcome to Bistro Boss Restaurant</h1>
+            <p>Enjoy our delicious meals and excellent service.</p>
+        </div>
+    );
+};
+
+export default Home;
