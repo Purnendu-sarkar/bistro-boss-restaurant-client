24f844104a25eb6ee6963a3b99a2a5444a07f411
Purnendu-sarkar <dsr102.purnendu@gmail.com>
2025-01-09 18:06:24 +0600

    create menu page and set dynamic page title based on route

---
+++ src/pages/Menu/Menu/Menu.jsx
@@ -0,0 +1,13 @@
+import { Helmet } from "react-helmet-async";
+const Menu = () => {
+  return (
+    <div>
+      <Helmet>
+        <title>Bistro Boss | Menu</title>
+      </Helmet>
+      <h1>Our Menu</h1>
+      <p>Check out our delicious offerings below!</p>
+    </div>
+  );
+};
+export default Menu;
