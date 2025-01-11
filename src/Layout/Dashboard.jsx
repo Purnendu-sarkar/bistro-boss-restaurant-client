3da4d2d18ca422d931fbc67aab559fcb8095c28a
Purnendu-sarkar <dsr102.purnendu@gmail.com>
2025-01-11 11:48:11 +0600

    display cart items on the cart page

--- src/Layout/Dashboard.jsx
+++ src/Layout/Dashboard.jsx
@@ -3,6 +3,7 @@ import {
   FaCalendar,
   FaHome,
   FaList,
+  FaSearch,
   FaShoppingCart,
 } from "react-icons/fa";
 import { NavLink, Outlet } from "react-router-dom";
@@ -42,10 +43,23 @@ const Dashboard = () => {
               My Bookings
             </NavLink>
           </li>
+          <div className="divider"></div>
+          <li>
+            <NavLink to="/">
+              <FaHome></FaHome>
+              Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/order/salad">
+              <FaSearch></FaSearch>
+              Menu
+            </NavLink>
+          </li>
         </ul>
       </div>
       {/* dashboard content */}
-      <div className="flex-1">
+      <div className="flex-1 p-8">
         <Outlet></Outlet>
       </div>
     </div>
